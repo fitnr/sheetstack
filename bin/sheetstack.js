0d092138c258bdc8e82aea6d54551c9ae4b24d5e
fitnr <fitnr@fakeisthenewreal>
2015-01-23 18:30:16 -0500

    fix option settings

--- bin/sheetstack.js
+++ bin/sheetstack.js
@@ -9,13 +9,16 @@ var j = require('j'),
 
 var version = '0.1.0';
 
+function list(val) { return val.split(','); }
+
 program
     .version(version)
     .usage('[OPTIONS] <file>\n  Combine XLS or XLSX sheets into a single CSV')
-    .option('-g, --groups <comma-separated list>', 'value of the field to be added at the start of each line (by default, the name of the sheet)')
-    .option('-r, --rm-lines <number>', 'number of lines to remove from the start of every sheet (except for the first). default: 1', 1)
-    .option('-n, --group-name <name>', 'name of grouping column. default: sheet', 'sheet')
-    .option('-F, --field-sep <char>', 'CSV field separator', ',')
+    .option('-s, --sheets <items>', 'list of sheets to read (by default, all sheets will be read', list)
+    .option('-g, --groups <items>', 'value of the field to be added at the start of each line (by default, the name of the sheet)', list)
+    .option('-r, --rm-lines <n>', 'number of lines to remove from the start of every sheet (except for the first). default: 1', 1)
+    .option('-n, --group-name <value>', 'name of grouping column. default: sheet', 'sheet')
+    .option('-F, --field-sep <sep>', 'CSV field separator', ',')
     .option('-R, --row-sep <sep>', 'CSV row separator', "\n")
     .option('-o, --output <file>', 'output to specified file')
     .option('-q, --quiet', 'quiet mode');
